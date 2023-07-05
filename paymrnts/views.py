d6bf26fb3a0f61b8fe1acd922c85a0fbbf2a805f
fitbouncer <bhuinjohn@gmail.com>
2023-07-05 21:24:04 +0530

    first commit

---
+++ paymrnts/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+
+# Create your views here.
+def firstFunction(request):
+    return HttpResponse ('Pradipta first Django')
+
+
+def paymentFunction(request):
+    a = 'this is payment page'
+    return HttpResponse(f'hi {a}')
+
+def calculateFunction(request):
+    a= 3
+    b = 4
+    c = a+b
+    return HttpResponse(f'The result is {c}. do again')
---
+++ product/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+def date(reqest):
+    return HttpResponse('this is product date function')
